c15d46ff6de19134784ba2908e3ecccd79d178bc
LeBalz <lebalz@outlook.com>
2023-03-22 23:34:22 +0100

    add more details on import

--- src/services/importExcel.ts
+++ src/services/importExcel.ts
@@ -2,6 +2,24 @@ import { EventState, Prisma } from "@prisma/client";
 import readXlsxFile from 'read-excel-file/node';
 import prisma from '../prisma';
 
+const COLUMNS = {
+  KW: 0,
+  weekday: 1,
+  description: 2,
+  startDate: 3,
+  startTime: 4,
+  endDate: 5,
+  endTime: 6,
+  location: 7,
+  affectedTeachers: 8,
+  gbsl: 9,
+  fms: 10,
+  wms: 11,
+  descriptionLong: 12,
+  classYears: 13,
+  classes: 14
+}
+
 const extractTime = (time: string): [number, number] => {
   if (!time) {
     return [0, 0];
@@ -18,19 +36,19 @@ const extractTime = (time: string): [number, number] => {
 export const importExcel = async (file: string, userId: string, jobId: string) => {
   const xlsx = await readXlsxFile(file);
   const imports = xlsx.slice(1).map(async (e) => {
-    const start = e[3] as any as Date;
-    const startTime = e[4] as string;
+    const start = e[COLUMNS.startDate] as any as Date;
+    const startTime = e[COLUMNS.startTime] as string;
     let allDay = !startTime;
     if (startTime) {
       const [hours, minutes] = extractTime(startTime);
       start.setUTCHours(hours);
       start.setUTCMinutes(minutes);
     }
-    let ende = e[5] as any as Date;
+    let ende = e[COLUMNS.endDate] as any as Date;
     if (!ende) {
       ende = start
     }
-    const endTime = e[6] as string;
+    const endTime = e[COLUMNS.endTime] as string;
     if (allDay) {
       if (!ende) {
         ende = new Date(start.getTime());
@@ -48,32 +66,46 @@ export const importExcel = async (file: string, userId: string, jobId: string) =
       where: Prisma.DepartmentWhereUniqueInput,
       create: (Prisma.Without<Prisma.DepartmentCreateWithoutEventsInput, Prisma.DepartmentUncheckedCreateWithoutEventsInput> & Prisma.DepartmentUncheckedCreateWithoutEventsInput)
     }[] = [];
-    if (e[9]) {
+    if (e[COLUMNS.gbsl]) {
       departments.push({
         create: { name: 'GBSL' },
         where: { name: 'GBSL' }
       });
     }
-    if (e[10]) {
+    if (e[COLUMNS.fms]) {
       departments.push({
         create: { name: 'FMS' },
         where: { name: 'FMS' }
       });
     }
-    if (e[11]) {
+    if (e[COLUMNS.wms]) {
       departments.push({
         create: { name: 'WMS' },
         where: { name: 'WMS' }
       });
     }
+    const classesRaw = e[COLUMNS.classes] as string || '';
+
+    /**
+     * \d matches a digit (equivalent to [0-9])
+     * \d matches a digit (equivalent to [0-9])
+     * \S matches any non-whitespace character
+     */
+    const classes = classesRaw.match(/(\d\d\S)/g)?.map((c) => c);
+
+    const classYearsRaw = (e[COLUMNS.classYears] as string || '').match(/(GYM|FMS|WMS)\d/g)?.map((c) => c) || [];
+    const classYears = classYearsRaw.map((c) => Number.parseInt(c.charAt(3), 10));
+
     return prisma.event.create({
       data: {
-        description: e[2] as string || '',
-        descriptionLong: e[12] as string || '',
-        location: e[7] as string || '',
+        description: e[COLUMNS.description] as string || '',
+        descriptionLong: e[COLUMNS.descriptionLong] as string || '',
+        location: e[COLUMNS.location] as string || '',
         start: start,
         end: ende || start,
         state: EventState.DRAFT,
+        classes: classes,
+        classYears: classYears,
         author: {
           connect: { id: userId }
         },
