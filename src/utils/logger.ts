daddcafb222d661160fb9bc499c7dad776793dab
LeBalz <lebalz@outlook.com>
2023-08-30 15:29:45 +0200

    add logger files

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -39,11 +39,11 @@ const format = winston.format.combine(
 
 const transports = [
   new winston.transports.Console(),
-  // new winston.transports.File({
-  //   filename: 'logs/error.log',
-  //   level: 'error',
-  // }),
-  // new winston.transports.File({ filename: 'logs/all.log' }),
+  new winston.transports.File({
+    filename: 'logs/error.log',
+    level: 'error',
+  }),
+  new winston.transports.File({ filename: 'logs/all.log' }),
 ]
 
 const Logger = winston.createLogger({
