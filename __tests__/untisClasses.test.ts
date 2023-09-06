d712245ca9e5ff54a6adce0b9b82b65a2279875a
LeBalz <lebalz@outlook.com>
2023-09-06 20:10:49 +0200

    add some simple tests

---
+++ __tests__/untisClasses.test.ts
@@ -0,0 +1,56 @@
+import { JobType, Prisma, Role, User } from "@prisma/client";
+import UntisClasses from "../src/models/untisClasses";
+import prismock from "../__mocks__/prismockClient";
+import { createUser } from "./users.test";
+import { HTTP400Error, HTTP403Error, HTTP404Error } from "../src/utils/errors/Errors";
+import { createUntisTeacher } from "./untisTeachers.test";
+import { PrismockClientType } from "prismock/build/main/lib/client";
+
+export const createUntisClass = async (props: Partial<Prisma.UntisClassUncheckedCreateInput>) => {
+    return await prismock.untisClass.create({
+        data: {
+            name: '25h',
+            sf: 'E',
+            year: 2025,
+            ...props
+        }
+    });
+}
+
+describe('UntisClass', () => {
+    describe('all', () => {
+        test('returns all untisClasses', async () => {
+            // const teacherA = await createUntisTeacher({ name: 'fba' });
+            // const teacherB = await createUntisTeacher({ name: 'fbb' });
+            // const teacherC = await createUntisTeacher({ name: 'fbc' });
+            const c24i = await createUntisClass({ name: '24i', 
+                // teachers: {
+                // connect: [
+                //     { id: teacherA.id },
+                //     { id: teacherC.id },
+                // ]
+                // }
+            });
+            const c25h = await createUntisClass({ name: '25h', 
+            // teachers: {
+                // connect: [
+                //     { id: teacherA.id },
+                //     { id: teacherB.id },
+                // ]
+            // }
+            });
+            await expect(UntisClasses.all()).resolves.toEqual([
+                {
+                    ...c24i,
+                    lessons: [],
+                    teachers: []
+                },
+                {
+                    ...c25h,
+                    lessons: [],
+                    teachers: []
+                }
+            ])
+        });
+    });
+});
---
+++ __tests__/untisTeachers.test.ts
@@ -0,0 +1,28 @@
+import { JobType, Prisma, Role, User } from "@prisma/client";
+import Semesters from "../src/models/semesters";
+import prismock from "../__mocks__/prismockClient";
+import { createUser } from "./users.test";
+import { HTTP400Error, HTTP403Error, HTTP404Error } from "../src/utils/errors/Errors";
+import { PrismockClientType } from "prismock/build/main/lib/client";
+
+export const createUntisTeacher = async (props: Partial<Prisma.UntisClassUncheckedCreateInput>) => {
+    return await prismock.untisTeacher.create({
+        data: {
+            active: true,
+            name: 'fba',
+            longName: 'Foo Bar',
+            title: 'M'
+        }
+    });
+}
+
+describe('UntisTeacher', () => {
+    describe('all', () => {
+        test('returns all untisClasses', async () => {
+            createUntisTeacher({ name: '25h' });
+            createUntisTeacher({ name: '24i' });
+            
+            
+        });
+    });
+});
