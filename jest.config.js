b38394fa55807c7acd65d5e4bb0f2dbf43b96fbd
LeBalz <lebalz@outlook.com>
2023-09-13 20:13:54 +0200

    do net report test folder

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,7 @@ module.exports = {
     clearMocks: true,
     preset: 'ts-jest',
     testEnvironment: 'node',
+    coverageReporters: ['json'],
+    coveragePathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/tests/'],
     maxWorkers: 1,
   }
