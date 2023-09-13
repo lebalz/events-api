ef8176debbcf0159cee3df62384e8c75d08c0464
LeBalz <lebalz@outlook.com>
2023-09-13 19:30:14 +0200

    fix wrong path

--- jest.config.unit.js
+++ jest.config.unit.js
@@ -1,8 +1,8 @@
 var config = require('./jest.config')
 config.testRegex = "\\.test\\.ts$" //Overriding testRegex option
 config.setupFilesAfterEnv = [
-  '<rootDir>/tests/unit_tests/__mocks__/prismockConfig.ts',
-  '<rootDir>/tests/unit_tests/__mocks__/prismockCleaner.ts'
+  '<rootDir>/tests/unit/__mocks__/prismockConfig.ts',
+  '<rootDir>/tests/unit/__mocks__/prismockCleaner.ts'
 ],
 console.log('RUNNING UNIT TESTS')
 
