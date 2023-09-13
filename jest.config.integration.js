a942acded4cfb88dfc967bb134c92bbec647d353
LeBalz <lebalz@outlook.com>
2023-09-13 20:01:42 +0200

    upload all cov reports

--- jest.config.integration.js
+++ jest.config.integration.js
@@ -1,5 +1,7 @@
 var config = require('./jest.config')
 config.testRegex = "\\.spec\\.ts$"
+
+config.coverageDirectory = 'coverage.integration',
 console.log('RUNNING INTEGRATION TESTS')
 
 module.exports = config
--- jest.config.unit.js
+++ jest.config.unit.js
@@ -4,6 +4,7 @@ config.setupFilesAfterEnv = [
   '<rootDir>/tests/unit/__mocks__/prismockConfig.ts',
   '<rootDir>/tests/unit/__mocks__/prismockCleaner.ts'
 ],
+config.coverageDirectory = 'coverage.unit',
 console.log('RUNNING UNIT TESTS')
 
 module.exports = config
