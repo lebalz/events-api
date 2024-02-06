b3b3bc3415e8657bd41fdad4585a23b87b3a3eb8
LeBalz <lebalz@outlook.com>
2024-02-06 10:09:09 +0100

    add users endpoint to the authenticated routes

--- src/routes/authConfig.ts
+++ src/routes/authConfig.ts
@@ -81,6 +81,13 @@ const authConfig: Config = {
                 roles: [Role.ADMIN, Role.USER],
             }]
         },
+        users: {
+            path: '/users',
+            access: [{
+                methods: ['GET'],
+                roles: [Role.ADMIN, Role.USER],
+            }]
+        },
         userEvents: {
             path: '/user/events',
             access: [{
