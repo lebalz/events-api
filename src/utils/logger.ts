4c559a531cb3c64f8c956c783742e4be3b9c0733
LeBalz <lebalz@outlook.com>
2024-05-06 20:51:41 +0200

    use console logger only

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -55,12 +55,7 @@ const format = winston.format.combine(
 )
 
 const transports = [
-  new winston.transports.Console(),
-  new winston.transports.File({
-    filename: 'logs/error.log',
-    level: 'error',
-  }),
-  new winston.transports.File({ filename: 'logs/all.log' }),
+  new winston.transports.Console()
 ]
 
 const Logger = winston.createLogger({
