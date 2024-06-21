7c7e35470e84594f78ec7f659c6dd23bad201df7
LeBalz <lebalz@outlook.com>
2024-06-21 14:50:22 +0200

    fix ical header replacement

--- src/services/createIcs.ts
+++ src/services/createIcs.ts
@@ -33,7 +33,7 @@ END:VTIMEZONE` as const;
 
 const withTimezoneHeader = (ics: string) => {
     /** insert before the first 'BEGIN:VEVENT' part */
-    return ics.replace(/BEGIN:VEVENT\n/, `${ICS_TIMEZONE_HEADER}\nBEGIN:VEVENT\n`);
+    return ics.replace('BEGIN:VEVENT', `${ICS_TIMEZONE_HEADER}\nBEGIN:VEVENT`);
 }
 
 export const toDateArray = (date: Date): DateArray => {
