49ee5d5f93d6a4d0af58e1467577468c75083ff0
LeBalz <lebalz@outlook.com>
2025-01-11 11:24:13 +0100

    make db cleanup more robust

--- tests/helpers/db.ts
+++ tests/helpers/db.ts
@@ -20,6 +20,7 @@ export const truncate = async (deleteFiles: boolean = true) => {
     }
 
     await prisma.$transaction([
+        prisma.subscription.deleteMany(),
         prisma.registrationPeriod.deleteMany(),
         prisma.department.deleteMany(),
         prisma.job.deleteMany(),
@@ -29,7 +30,6 @@ export const truncate = async (deleteFiles: boolean = true) => {
         prisma.untisTeacher.deleteMany(),
         prisma.event.deleteMany(),
         prisma.eventGroup.deleteMany(),
-        prisma.subscription.deleteMany(),
         prisma.user.deleteMany()
     ]);
 };
