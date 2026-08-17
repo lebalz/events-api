ac1c65ec409f3b9d86a371805aeec6bfc2061a2a
bh0fer <lebalz@outlook.com>
2026-08-17 13:56:38 +0000

    format code base

--- src/auth.ts
+++ src/auth.ts
@@ -64,27 +64,26 @@ export const auth = betterAuth({
     socialProviders: {
         ...(HAS_PROVIDER_MSFT
             ? {
-                microsoft: {
-                    clientId: process.env.MSAL_CLIENT_ID as string,
-                    clientSecret: process.env.MSAL_CLIENT_SECRET as string,
-                    tenantId: process.env.MSAL_TENANT_ID || 'common', // Use 'common' for multi-tenant applications
-                    authority: 'https://login.microsoftonline.com', // Authentication authority URL
-                    prompt: 'select_account', // Forces account selection,
-                    responseMode: 'query',
-                    mapProfileToUser: (profile) => {
-                        const email = (profile.email || profile.preferred_username)?.toLowerCase();
-                        const name = getNameFromMsftProfile(profile);
-                        return {
-                            id: profile.oid,
-                            email: email,
-                            firstName: name.firstName || '',
-                            lastName: name.lastName || ''
-                            // You can extract and map other fields as needed
-                        };
-                    },
-
-                }
-            }
+                  microsoft: {
+                      clientId: process.env.MSAL_CLIENT_ID as string,
+                      clientSecret: process.env.MSAL_CLIENT_SECRET as string,
+                      tenantId: process.env.MSAL_TENANT_ID || 'common', // Use 'common' for multi-tenant applications
+                      authority: 'https://login.microsoftonline.com', // Authentication authority URL
+                      prompt: 'select_account', // Forces account selection,
+                      responseMode: 'query',
+                      mapProfileToUser: (profile) => {
+                          const email = (profile.email || profile.preferred_username)?.toLowerCase();
+                          const name = getNameFromMsftProfile(profile);
+                          return {
+                              id: profile.oid,
+                              email: email,
+                              firstName: name.firstName || '',
+                              lastName: name.lastName || ''
+                              // You can extract and map other fields as needed
+                          };
+                      }
+                  }
+              }
             : {})
     },
     trustedOrigins: CORS_ORIGIN_STRINGIFIED,
@@ -96,13 +95,13 @@ export const auth = betterAuth({
         },
         cookies: process.env.NETLIFY_PROJECT_NAME
             ? {
-                session_token: {
-                    attributes: {
-                        sameSite: 'none',
-                        secure: true
-                    }
-                }
-            }
+                  session_token: {
+                      attributes: {
+                          sameSite: 'none',
+                          secure: true
+                      }
+                  }
+              }
             : undefined,
         database: { generateId: false, useNumberId: false }
     },
@@ -112,7 +111,7 @@ export const auth = betterAuth({
                 before: async (user) => {
                     if (!IsValidEmail.test(user.email)) {
                         // Perform any necessary actions for edu emails
-                        throw new Error("Only @gbsl.ch or @gbjb.ch email addresses are allowed.");
+                        throw new Error('Only @gbsl.ch or @gbjb.ch email addresses are allowed.');
                     }
                     return { data: user };
                 }
